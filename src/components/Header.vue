bfe96be30871191bd2c17d82bb67f1b4abe82a36
eduardo-gualberto <eduardo.santos.gualberto@gmail.com>
2020-10-09 14:32:05 -0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="header-component">
+    <div class="header-el">
+      <p>alow</p>
+      <p>jovem</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header-el {
+  display: flex;
+  flex-direction: row;
+  font-size: 30px;
+  margin: auto;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+//import do framework do bootstrap-vue
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+Vue.use(BootstrapVue);
+
+import { IconsPlugin } from "bootstrap-vue";
+Vue.use(IconsPlugin);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
