f3cb637c92a103fa86c9f2e65466b5960df91614
Iury Gomes de Oliveira <iury.oliveira@ifto.edu.br>
2020-10-11 23:45:26 -0300

    Habilitando CORS

--- back/www/index.php
+++ back/www/index.php
@@ -5,6 +5,7 @@ ob_start();
 
 require __DIR__ . "/vendor/autoload.php";
 
+use Slim\Exception\HttpNotFoundException;
 use Slim\Factory\AppFactory;
 use Api\Controllers\Payment;
 use Api\Controllers\Access;
@@ -16,6 +17,19 @@ if (IS_DEV_MODE) {
     ini_set("display_errors", 1);
 }
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+
 // ROUTES ###########################################
 
 // Create Authentication
@@ -24,6 +38,9 @@ $app->post('/authentication', Payment::class . ':Authentication');
 // Create and Check Login
 $app->post('/login', Access::class . ':login');
 
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($request, $response) {
+    throw new HttpNotFoundException($request);
+});
 
 // RUN ###########################################
 
